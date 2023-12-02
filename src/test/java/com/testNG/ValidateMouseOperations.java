2daac74a18c4c248cf08ed5ec9f7d8867e92a58e
Koripriyan <koripriyan@gmail.com>
2023-12-02 19:10:37 +0530

    TestNG to VerifyRightClickOperation

--- src/test/java/com/testNG/ValidateMouseOperations.java
+++ src/test/java/com/testNG/ValidateMouseOperations.java
@@ -1,10 +1,14 @@
 package com.testNG;
 
 import org.testng.annotations.Test;
+
+import com.utility.Library;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -20,40 +24,49 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
-public class ValidateMouseOperations {
-  public WebDriver driver; 
+public class ValidateMouseOperations extends Library {
+	
+// public WebDriver driver; 
+// public Properties objProperties;
   
   @Test(priority=1)
   public void ValidateJQueryPageLoadedSuccessfully() throws InterruptedException
   {
+
+	  driver.get(objProperties.getProperty("mouseOpeartionRightClickURL"));
+	  PageLoadTimeOut();
 	  System.out.println("ValidateJQueryPageLoadedSuccessfully");
 	  String TitleOfJqueryPage =  driver.getTitle();
 	  Assert.assertEquals(TitleOfJqueryPage,"jQuery contextMenu (2.x)");
 	  
-	  //Thread.sleep(2000);
+	  //Thread.sleep(2000);	    
+  }
+  
+  @Test(priority=2)
+  public void VerifyRightClickOperation() throws InterruptedException 
+  {
+	  System.out.println("Inside verify right click operation");     
 	  Actions objActions = new Actions(driver);	  
 	  WebElement element =  driver.findElement(By.xpath("//span[contains(text(),'right')]"));
-	  objActions.contextClick(element).build().perform();
-	  
-	  
-	  WebElement Paste = driver.findElement(By.xpath("//li/span[contains(text(),'Paste')]"));
-	  
-	  
+	  objActions.contextClick(element).build().perform();	  
+	  WebElement Paste = driver.findElement(By.xpath("//span[text()='Paste']")); 
 	  objActions.click(Paste).build().perform();
+	  Thread.sleep(2000);
 	  Alert objAlert = driver.switchTo().alert();	  
 	  String AlertText = objAlert.getText();
 	  System.out.println("Alert Text:"+AlertText);
 	  Assert.assertEquals(AlertText, "clicked: paste");
-	  objAlert.accept();	  
+	  objAlert.accept();		  
   }
   
-  @Test(priority=2,enabled=false)
-  public void VerifyRightClickOperation() 
+  
+  @Test(priority=3)
+  public void VerifyDoubleClickOperation() throws InterruptedException 
   {
-	  System.out.println("Inside verify right click operation");
-
-	  
+	  	  
   }
+  
+  
   
   @BeforeMethod
   public void beforeMethod() {
@@ -88,11 +101,8 @@ public class ValidateMouseOperations {
   @BeforeSuite
   public void beforeSuite() {
 	    System.out.println("Inside Before Suite");
-	    System.setProperty("webdriver.chrome.driver","D:\\JavaWorkSpace\\Selenium_LiveCourse_2204101110\\src\\test\\resources\\chromedriver-v119\\chromedriver.exe");
-	    driver = new ChromeDriver();
-	    driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
-	    driver.manage().window().maximize();
-	    driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+	    ReadPropertiesFile();
+	    LaunchBrowser();
   }
 
   @AfterSuite
